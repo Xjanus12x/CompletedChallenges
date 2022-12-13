ff8cb9a737a93b8989944eb4841c9e31d237813f
Xjanus12x <70027689+Xjanus12x@users.noreply.github.com>
2022-12-13 13:15:59 +0800

    Add files via upload

--- intro-section-with-dropdown-navigation-main/functionality.js
+++ intro-section-with-dropdown-navigation-main/functionality.js
@@ -1,21 +1,32 @@
-const openMenu = document.querySelector(".open-menu");
-const closeMenu = document.querySelector(".close-menu");
+const primaryNavigationOpenButton = document.querySelector(".open-menu");
+const primaryNavigationCloseButton = document.querySelector(".close-menu");
 const primaryNavigation = document.getElementById("primary-navigation");
+const secondaryNavigation = document.getElementById("secondary-navigation");
+const secondaryNavigationButton = document.getElementById(
+  "toggle-secondary-navigation"
+);
+const tertiaryNavigation = document.getElementById("tertiary-navigation");
+const tertiaryNavigationButton = document.getElementById(
+  "tertiary-toggle-menu"
+);
 const body = document.querySelector("body");
 
-openMenu.addEventListener("click", () => {
-  const isNotOpened = openMenu.getAttribute("aria-expanded") === "false";
+// Primary navigation functionality
+primaryNavigationOpenButton.addEventListener("click", () => {
+  const isNotOpened =
+    primaryNavigationOpenButton.getAttribute("aria-expanded") === "false";
   if (isNotOpened) {
-    openMenu.setAttribute("aria-expanded", "true");
+    primaryNavigationOpenButton.setAttribute("aria-expanded", "true");
     body.classList.add("semi-dark");
   }
   primaryNavigation.setAttribute("data-state", "opened");
 });
 
-closeMenu.addEventListener("click", () => {
-  const isOpened = openMenu.getAttribute("aria-expanded") === "true";
+primaryNavigationCloseButton.addEventListener("click", () => {
+  const isOpened =
+    primaryNavigationOpenButton.getAttribute("aria-expanded") === "true";
   if (isOpened) {
-    openMenu.setAttribute("aria-expanded", "false");
+    primaryNavigationOpenButton.setAttribute("aria-expanded", "false");
     body.classList.remove("semi-dark");
   }
   primaryNavigation.setAttribute("data-state", "closing");
@@ -28,3 +39,29 @@ closeMenu.addEventListener("click", () => {
     { once: true }
   );
 });
+
+// Secondary navigation functionality
+menuEventListener(secondaryNavigationButton, secondaryNavigation);
+
+// Tertiary navigation functionality
+menuEventListener(tertiaryNavigationButton, tertiaryNavigation);
+
+// event listener in a function
+function menuEventListener(activeButton, navigationName) {
+  activeButton.addEventListener("click", () => {
+    const isOpened = activeButton.getAttribute("aria-expanded") === "true";
+    isOpened
+      ? closeMenu(activeButton, navigationName)
+      : openMenu(activeButton, navigationName);
+  });
+}
+
+function openMenu(activeButton, navigationName) {
+  navigationName.setAttribute("data-state", "opened");
+  activeButton.setAttribute("aria-expanded", "true");
+}
+
+function closeMenu(activeButton, navigationName) {
+  navigationName.setAttribute("data-state", "closed");
+  activeButton.setAttribute("aria-expanded", "false");
+}
