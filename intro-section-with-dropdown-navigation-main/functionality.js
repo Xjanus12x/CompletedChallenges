2d3f246b79393700c7c7d236a2df1381ed5d43aa
Xjanus12x <70027689+Xjanus12x@users.noreply.github.com>
2022-12-14 23:41:51 +0800

    Add files via upload

--- intro-section-with-dropdown-navigation-main/functionality.js
+++ intro-section-with-dropdown-navigation-main/functionality.js
@@ -9,8 +9,10 @@ const tertiaryNavigation = document.getElementById("tertiary-navigation");
 const tertiaryNavigationButton = document.getElementById(
   "tertiary-toggle-menu"
 );
+
 const body = document.querySelector("body");
 
+let isOldImage = true;
 // Primary navigation functionality
 primaryNavigationOpenButton.addEventListener("click", () => {
   const isNotOpened =
@@ -41,27 +43,43 @@ primaryNavigationCloseButton.addEventListener("click", () => {
 });
 
 // Secondary navigation functionality
-menuEventListener(secondaryNavigationButton, secondaryNavigation);
+menuEventListener(secondaryNavigationButton, secondaryNavigation, 1);
 
 // Tertiary navigation functionality
-menuEventListener(tertiaryNavigationButton, tertiaryNavigation);
+menuEventListener(tertiaryNavigationButton, tertiaryNavigation, 2);
 
 // event listener in a function
-function menuEventListener(activeButton, navigationName) {
+function menuEventListener(activeButton, navigationName, groupNumberOfIcons) {
   activeButton.addEventListener("click", () => {
     const isOpened = activeButton.getAttribute("aria-expanded") === "true";
     isOpened
-      ? closeMenu(activeButton, navigationName)
-      : openMenu(activeButton, navigationName);
+      ? closeMenu(activeButton, navigationName, groupNumberOfIcons)
+      : openMenu(activeButton, navigationName, groupNumberOfIcons);
   });
 }
 
-function openMenu(activeButton, navigationName) {
+function openMenu(activeButton, navigationName, groupNumberOfIcons) {
   navigationName.setAttribute("data-state", "opened");
   activeButton.setAttribute("aria-expanded", "true");
+  changeImage(groupNumberOfIcons, true);
 }
 
-function closeMenu(activeButton, navigationName) {
+function closeMenu(activeButton, navigationName, groupNumberOfIcons) {
   navigationName.setAttribute("data-state", "closed");
   activeButton.setAttribute("aria-expanded", "false");
+  changeImage(groupNumberOfIcons, false);
+}
+
+function changeImage(groupNumberOfIcons, isOpened) {
+  const iconContainer = document.getElementById(
+    `set${groupNumberOfIcons}icons`
+  ).childNodes;
+
+  if (isOpened) {
+    iconContainer[1].style.display = "none";
+    iconContainer[3].style.display = "block";
+  } else {
+    iconContainer[1].style.display = "block";
+    iconContainer[3].style.display = "none";
+  }
 }
